fec736808cfdafebc8b19c4c1b7d6d6d975ee9b0
NikethJoy <nikethjoy@gmail.com>
2023-03-31 20:17:53 +0530

    Initial commit

---
+++ basic1.js
@@ -0,0 +1,16 @@
+// console.log("hello world");
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+//  }
+
+// let s=0;
+// for(let i=0;i<=10;i++){
+//     s=s+i;
+// }
+// console.log(s);
+
+let students = ["x","y","z"]
+for(let student of students){
+    console.log(`hello ${student}`)
+    }
